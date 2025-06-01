7c35b43c9b897f953ec2ce4fe0cdda4776f306af
Favizille <favourgabrirl50@gmail.com>
2025-06-01 23:43:44 +0100

    added the welcome blade, master layout and farme rlogin blade

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login', function () {
+    return view('farmer_login');
 });
+
+
+Route::get('/', [ProductController::class, 'getAllProducts'])->name('products.all');
